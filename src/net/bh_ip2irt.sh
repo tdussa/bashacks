32830ee8e4d55ab09c9d8de78858518be973b62e
Tobias Dussa <dussa@dfn-cert.de>
2021-02-26 15:39:36 +0100

    Made ip2irt call more robust (addressing #1).

--- src/net/bh_ip2irt.sh
+++ src/net/bh_ip2irt.sh
@@ -2,11 +2,30 @@ bh_ip2irt()
 {
     local ipaddress="${1:-`bh_myip`}"
 
-    local whoisresult="$(whois -c "$ipaddress")"
-   
-    echo "${whoisresult}" \
-        | sed '/ [Aa]buse contact /!d'
-    echo
-    echo "${whoisresult}" \
-        | sed '/^irt:/,/^$/!d'
+    local whoiscresult="$(whois -c "$ipaddress")"
+    local whoisresult="$(whois "$ipaddress")"
+
+    local whoiscabuse=$(echo "${whoiscresult}" \
+	                    | fgrep -i " abuse contact ")
+    local whoiscirt=$(echo "${whoiscresult}" \
+                          | sed '/^irt:/,/^$/!d')
+    local whoisabuse=$(echo "${whoisresult}" \
+                           | fgrep -i "abuse")
+    local whoisirt=$(echo "${whoisresult}" \
+                         | sed '/^irt:/,/^$/!d')
+
+    if [ -n "${whoiscabuse}" ] \
+       && ! echo "${whoiscabuse}" \
+              | fgrep -qc "% No abuse contact registered for "; then
+        echo -e "${whoiscabuse}\n"
+    fi
+    if [ -n "${whoiscirt}" ]; then
+	    echo -e "${whoiscirt}\n"
+    fi
+    if [ -n "${whoisabuse}" ]; then
+	    echo -e "${whoisabuse}\n"
+    fi
+    if [ -n "${whoisirt}" ]; then
+	    echo -e "${whoisirt}\n"
+    fi
 }
