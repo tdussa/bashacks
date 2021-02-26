964ae99b8d568ba78fe2ac9383d98d2b8111fc87
Tobias Dussa <tobias@dussa.de>
2021-02-26 08:04:51 +0100

    Reverted unnecessary change to bh_ipinfo.

--- src/net/bh_ipinfo.sh
+++ src/net/bh_ipinfo.sh
@@ -3,7 +3,7 @@ bh_ipinfo()
     local ipaddress="${1:-`bh_myip`}"
     local url="http://ipinfo.io"
 
-    local company=$(bh_cmd_wget -qO - "$url/$ipaddress/org") 
+    local company"=$(bh_cmd_wget -qO - "$url/$ipaddress/org")"
     local asreg="${company%% *}"
 
     echo "$company"
